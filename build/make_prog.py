7fc7bba0fb2cd9e5043f6d9bb72c613581509f6b
Mark Vasilkov <mvasilkov@gmail.com>
2017-04-02 16:15:48 +0300

    make_prog.py

---
+++ build/make_prog.py
@@ -0,0 +1,45 @@
+from glob import iglob
+import json
+from os import makedirs
+from os.path import isfile, split as split_path
+import re
+from shutil import rmtree
+
+PROGRAMS_ROOT = 'programs/'
+PROGRAMS = (p for p in iglob(PROGRAMS_ROOT + '**', recursive=True)
+            if isfile(p))
+OUT_DIR = 'build/prog'
+
+
+def mkdirp(path):
+    try:
+        makedirs(path)
+    except OSError:
+        pass
+
+
+def build(p):
+    assert p.startswith(PROGRAMS_ROOT)
+
+    lines = open(p, encoding='utf-8').readlines()
+    for n, line in enumerate(lines):
+        assert line.endswith('\n')
+        lines[n] = line[:-1]
+
+    p = p[len(PROGRAMS_ROOT):]
+    path, name = split_path(p)
+    path = '%s/%s' % (OUT_DIR, path)
+    mkdirp(path)
+
+    name = re.sub('(?<=\.).*?$', 'json', name)
+    with open('%s/%s' % (path, name), 'w', encoding='utf-8') as out:
+        out.write(json.dumps({'lines': lines}, indent=2))
+
+
+if __name__ == '__main__':
+    rmtree(OUT_DIR)
+    mkdirp(OUT_DIR)
+
+    for p in PROGRAMS:
+        print(p)
+        build(p)
---
+++ programs/name_main/javascript.js
@@ -0,0 +1,7 @@
+function main() {
+    console.log('hello, world')
+}
+
+if (require.main === module) {
+    main()
+}
