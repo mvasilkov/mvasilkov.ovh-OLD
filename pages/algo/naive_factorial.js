df6f4cea39b6540efaf21080dfd158bf287701df
Mark Vasilkov <mvasilkov@gmail.com>
2017-05-14 18:44:41 +0300

    Update

--- pages/algo/naive_factorial.js
+++ pages/algo/naive_factorial.js
@@ -12,8 +12,8 @@ class Content extends React.Component {
             <Article title={pageTitle}>
                 <header>
                     <h1>{pageTitle}</h1>
-                    {/* <p>As opposed to an efficient one</p> */}
                 </header>
+                <p>In order of decreasing performance:</p>
                 <h4>An iterative factorial function with an accumulator variable</h4>
                 <Listing program={prog} region="factorial" />
                 <h4>A recursive factorial function</h4>
