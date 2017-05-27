00479f35d2ae2037d79b64afab3ce12a84765487
я котик пур-пур <mvasilkov@gmail.com>
2017-05-28 01:15:17 +0300

    Quine

---
+++ test/test_quine.js
@@ -0,0 +1,27 @@
+import eol from 'eol'
+import fs from 'fs'
+import sh from 'shelljs'
+import test from 'ava'
+
+const python = sh.which('python3') ? 'python3' : 'python'
+const quineJS = 'programs/basic/quine.js'
+const quinePython = 'programs/basic/quine.py'
+
+test('Python quine', t => {
+    const a = sh.exec(`${python} ${quinePython}`, { silent: true })
+    t.is(a.code, 0)
+    const b = fs.readFileSync(quinePython, { encoding: 'utf-8' })
+    t.is(a.stdout, b)
+})
+
+test('JS quine', t => {
+    const a = sh.exec(`node ${quineJS}`, { silent: true })
+    t.is(a.code, 0)
+
+    if (process.platform.startsWith('win')) {
+        a.stdout = eol.crlf(a.stdout)
+    }
+
+    const b = fs.readFileSync(quineJS, { encoding: 'utf-8' })
+    t.is(a.stdout, b)
+})
